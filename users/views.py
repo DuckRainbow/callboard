b356867840a820c1e5cabca489f518a76373cd5c
DuckRainbow <sillestyna@gmail.com>
2024-12-17 12:34:37 +0500

    views users

--- users/views.py
+++ users/views.py
@@ -1,77 +1,114 @@
-from django.shortcuts import render
-
-import secrets
-
+from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import send_mail
-from django.shortcuts import get_object_or_404, redirect, render
-from django.urls import reverse_lazy, reverse
-from django.views.generic import CreateView, TemplateView, UpdateView, DetailView
-
-from users.forms import UserRegisterForm, UserUpdateForm
-from users.models import User
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from rest_framework.generics import GenericAPIView, CreateAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from config.settings import EMAIL_HOST_USER
+from users.models import User
+from users.serializers import UserSerializer, PasswordResetRequestSerializer, PasswordResetSerializer
 
 
-class UserCreateView(CreateView):
-    model = User
-    form_class = UserRegisterForm
-    success_url = reverse_lazy('users:login')
+class UserCreateAPIView(CreateAPIView):
+    """Класс для создания пользователя"""
 
-    def form_valid(self, form):
-        user = form.save()
-        user.is_active = False
-        token = secrets.token_hex(16)
-        user.token = token
-        user.save()
-        host = self.request.get_host()
-        url = f'http://{host}/users/email-confirm/{token}/'
-        send_mail(
-            subject='Подтверждение почты',
-            message=f'Привет, перейди по ссылке для подтверждения почты {url}',
-            from_email=EMAIL_HOST_USER,
-            recipient_list=[user.email],
-        )
-        return super().form_valid(form)
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save(is_active=True)
+            user.set_password(user.password)
+            user.save()
 
 
-def email_verification(request, token):
-    user = get_object_or_404(User, token=token)
-    user.is_active = True
-    user.save()
-    return redirect(reverse('users:login'))
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request):
+        username = request.data.get("email")
+        password = request.data.get("password")
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+
+            return Response(
+                {
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                }
+            )
+        else:
+            return Response("Неверные учетные данные, повторите попытку")
 
 
-class UserDetailView(DetailView):
-    model = User
+class PasswordResetRequestView(GenericAPIView):
+    """Запрос ссылки на email для смены пароля пользователя"""
 
-    def get_object(self, queryset=None):
-        return self.request.user
+    serializer_class = PasswordResetRequestSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            email = serializer.data["email"]
+
+            user = get_user_model().objects.get(email=email)
+            token = PasswordResetTokenGenerator().make_token(user)
+            uid = urlsafe_base64_encode(force_bytes(user.token))
+            user.token = token
+            user.uid = uid
+            user.save()
+            url = get_current_site(request)
+            confirm_url = "users/reset_password_confirm"
+            # Отправляем письмо со ссылкой для сброса пароля
+            send_mail(
+                subject="Запрос сброса пароля с сайта {}".format(url),
+                message="Для сброса пароля перейдите по ссылке: http://{}/{}/{}/{}/".format(
+                    url, confirm_url, uid, token
+                ),
+                from_email=EMAIL_HOST_USER,
+                recipient_list=[email],
+                fail_silently=False,
+            )
+
+            return Response(
+                "Ссылка для сброса пароля отправлена на вашу электронную почту."
+            )
+
+        else:
+            return Response(
+                "Введен неверный адрес электронной почты, повторите попытку."
+            )
 
 
-class UserUpdateView(UpdateView):
-    model = User
-    form_class = UserUpdateForm
-    success_url = reverse_lazy('users:profile')
+class PasswordResetView(GenericAPIView):
+    """Меняем пароль пользователя"""
 
-    def get_object(self, queryset=None):
-        return self.request.user
-
-
-class ResetPassword(TemplateView):
-    def get(self, request):
-        return render(request, 'users/reset_password.html')
+    serializer_class = PasswordResetSerializer
+    permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
-        email = request.POST.get('email')
-        user = get_object_or_404(User, email=email)
-        new_password = secrets.token_hex(8)
-        user.set_password(new_password)
-        user.save()
-        send_mail(
-            subject='Изменение пароля',
-            message=f'Твой новый пароль: {new_password}',
-            from_email=EMAIL_HOST_USER,
-            recipient_list=[user.email],
-        )
-        return redirect(reverse('users:login'))
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            token = self.kwargs.get("token")
+            object_reset = User.objects.filter(token=token).first()
+            if not object_reset:
+                return Response("Неверный токен", status=400)
+            uid = self.kwargs.get("uid")
+            user = User.objects.filter(uid=uid).first()
+            if user:
+                password = request.data["new_password"]
+                user.set_password(password)
+                user.token = None
+                user.uid = None
+                user.save()
+                return Response("Пароль успешно обновлен")
+            else:
+                return Response("Пользователь не найден")
